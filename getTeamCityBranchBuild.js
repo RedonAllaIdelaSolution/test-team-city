57cb975f2c2c2a763fffade360bb48b61b264775
RedonAllaIdelaSolution <43534908+RedonAllaIdelaSolution@users.noreply.github.com>
2018-10-03 16:28:47 +0200

    Update getTeamCityBranchBuild.js
    rt

--- getTeamCityBranchBuild.js
+++ getTeamCityBranchBuild.js
@@ -3,6 +3,6 @@ const argv = require('yargs').argv
  
 console.log('build branch: ', argv.branch)
 
-console.log("##teamcity[setParameter name='env.Test' value='test_value 1']");
+console.log("##teamcity[setParameter name='env.Test' value='MYVALUE']");
 console.log("##teamcity[buildNumber 'test_value 1']");
 console.log("##teamcity[message text='Test -> "+ argv.Test +"' status='NORMAL']");
