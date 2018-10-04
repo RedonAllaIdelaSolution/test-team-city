f459c8ee7a2c6fcaeb8341ae5bcdd8e9670cea43
RedonAllaIdelaSolution <43534908+RedonAllaIdelaSolution@users.noreply.github.com>
2018-10-04 10:12:54 +0200

    Update getTeamCityBranchBuild.js
    s

--- getTeamCityBranchBuild.js
+++ getTeamCityBranchBuild.js
@@ -6,7 +6,7 @@ var name = "Test"
 var value = "MyValue"
 console.log("##teamcity[setParameter name='env." + name + "' value='ParamTestValue']");
 console.log("##teamcity[setParameter name='env.MyParam' value='MyParamValue']");
-console.log("##teamcity[buildNumber 'test_value 1']");
+//console.log("##teamcity[buildNumber 'test_value 1']");
 
 console.log("##teamcity[message text='Test -> "+ argv.Test +"' status='NORMAL']");
 console.log("##teamcity[message text='MyParam -> "+ argv.MyParam +"' status='NORMAL']");
