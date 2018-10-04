4fbe94d952aed44e8f206ad59532a49e327bbbdb
RedonAllaIdelaSolution <43534908+RedonAllaIdelaSolution@users.noreply.github.com>
2018-10-04 13:50:30 +0200

    Update getTeamCityBranchBuild.js
    sd

--- getTeamCityBranchBuild.js
+++ getTeamCityBranchBuild.js
@@ -5,7 +5,7 @@ console.log('build branch: ', argv.branch)
 var name = "Test"
 var value = "MyValue"
 console.log("##teamcity[setParameter name='env." + name + "' value='ParamTestValue']");
-console.log("##teamcity[setParameter name='env.MyParam' value='MyParamValue']");
+console.log("##teamcity[setParameter name='env.DeployFTPHostname' value='ftp://waws-prod-dm1-025.ftp.azurewebsites.windows.net']");
 //console.log("##teamcity[buildNumber 'test_value 1']");
 
 console.log("##teamcity[message text='Test -> "+ argv.Test +"' status='NORMAL']");
